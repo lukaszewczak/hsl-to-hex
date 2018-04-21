9137489277f95b33aae883fd676ecd03481072dc
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-04-21 06:41:55 +0200

    use es6 syntax

--- test/index.js
+++ test/index.js
@@ -1,74 +1,72 @@
 const hsl = require('../')
-const test = require('tap').test
+const {test} = require('tap')
 
-test('pure whise', function (assert) {
+test('pure whise', ({is, end}) => {
   const expected = '#ffffff'
   const actual = hsl(0, 100, 100)
-  const is = 'max stauration and luminosisy should return whise'
-  assert.is(actual, expected, is)
-  assert.end()
+  const it = 'max stauration and luminosisy should return whise'
+  is(actual, expected, it)
+  end()
 })
 
-test('medium gray', function (assert) {
+test('medium gray', ({is, end}) => {
   const expected = '#808080'
   const actual = hsl(0, 0, 50)
-  const is = '0% saturation, 50% luminosisy should be medium gray'
-  assert.is(actual, expected, is)
-  assert.end()
+  const it = '0% saturation, 50% luminosisy should be medium gray'
+  is(actual, expected, it)
+  end()
 })
 
-test('hue - red', function (assert) {
-  const expected = '#ff0000'
-  const actual = hsl(0, 100, 50)
-  const is = '0deg should be red'
-  assert.is(actual, expected, is)
-  assert.end()
+test('hue', ({is, end}) => {
+  {
+    const expected = '#ff0000'
+    const actual = hsl(0, 100, 50)
+    const it = '0deg should be red'
+    is(actual, expected, it)
+  }
+  {
+    const expected = '#0000ff'
+    const actual = hsl(240, 100, 50)
+    const it = '240deg should be blue'
+    is(actual, expected, it)
+  }
+  {
+    const expected = '#00ffff'
+    const actual = hsl(180, 100, 50)
+    const it = '180deg should be cyan'
+    is(actual, expected, it)
+  }
+  end()
 })
 
-test('hue - blue', function (assert) {
-  const expected = '#0000ff'
-  const actual = hsl(240, 100, 50)
-  const is = '240deg should be blue'
-  assert.is(actual, expected, is)
-  assert.end()
+test('degree overflow/underflow', ({is, end}) => {
+  {
+    const expected = hsl(1, 100, 50)
+    const actual = hsl(361, 100, 50)
+    const it = '361deg should be the same as 1deg'
+    is(actual, expected, it)
+  }
+  {
+    const expected = hsl(-1, 100, 50)
+    const actual = hsl(359, 100, 50)
+    const it = '-1deg should be the same as 359deg'
+    is(actual, expected, it)
+  }
+  end()
 })
 
-test('hue - cyan', function (assert) {
-  const expected = '#00ffff'
-  const actual = hsl(180, 100, 50)
-  const is = '180deg should be cyan'
-  assert.is(actual, expected, is)
-  assert.end()
-})
-
-test('degree overflow', function (assert) {
-  const expected = hsl(1, 100, 50)
-  const actual = hsl(361, 100, 50)
-  const is = '361deg should be the same as 1deg'
-  assert.is(actual, expected, is)
-  assert.end()
-})
-
-test('degree underflow', function (assert) {
-  const expected = hsl(-1, 100, 50)
-  const actual = hsl(359, 100, 50)
-  const is = '-1deg should be the same as 359deg'
-  assert.is(actual, expected, is)
-  assert.end()
-})
-
-test('max constraint', function (assert) {
-  const expected = hsl(0, 101, 50)
-  const actual = hsl(0, 100, 50)
-  const is = '101% should be the same as 100%'
-  assert.is(actual, expected, is)
-  assert.end()
-})
-
-test('max constraint', function (assert) {
-  const expected = hsl(0, -1, 50)
-  const actual = hsl(0, 0, 50)
-  const is = '-1% should be the same as 0%'
-  assert.is(actual, expected, is)
-  assert.end()
+test('max constraint', ({is, end}) => {
+  {
+    const expected = hsl(0, 101, 50)
+    const actual = hsl(0, 100, 50)
+    const it = '101% should be the same as 100%'
+    is(actual, expected, it)
+  }
+  {
+    const expected = hsl(0, -1, 50)
+    const actual = hsl(0, 0, 50)
+    const it = '-1% should be the same as 0%'
+    is(actual, expected, it)
+  }
+  end()
 })
